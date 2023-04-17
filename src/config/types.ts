575548d2b47da678cf95d34889fde192efe2921b
TheLe0 <leob.tosin@hotmail.com>
2023-04-16 21:59:54 -0300

    wip: Start CosmosDB configurations

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,7 +1,16 @@
 export enum DatabaseType {
-    IN_MEMORY = 1
+    IN_MEMORY = 1,
+    COSMOS = 2
 }
 
 export interface AppConfig {
     databaseType: DatabaseType;
+    cosmosDBConnection: CosmosDBConfig;
+}
+
+export interface CosmosDBConfig {
+    endpoint: string;
+    key: string;
+    database: string;
+    container: string;
 }
